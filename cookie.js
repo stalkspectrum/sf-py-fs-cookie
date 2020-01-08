c518209c1f6cef1c41db1c4d9f11e0abbf382ddc
stalkspectrum <55536946+stalkspectrum@users.noreply.github.com>
2020-01-08 23:36:25 +0800

    Fix 6

--- cookie.js
+++ cookie.js
@@ -2,17 +2,43 @@ function jQuery (selector, context = document) {
     this.elements = Array.from(context.querySelectorAll(selector));
     return this;
 }
-
 jQuery.prototype.each = function(fn) {
     this.elements.forEach((element, index) => fn.call(element, element, index));
     return this;
 }
-
 jQuery.prototype.click = function(fn) {
     this.each(element => element.addEventListener('click', fn));
     return this
 }
+jQuery.prototype.hide = function() {
+    this.each(element => element.style.display = 'none');
+    return this;
+}
+jQuery.prototype.show = function() {
+    this.each(element => element.style.display = '');
+    return this;
+}
+jQuery.prototype.html = function(newHtml) {
+    if (newHtml) {
+        this.each(element => element.innerHTML = newHtml);
+        return this;
+    }
+    return this.elements[0].innerHTML;
+}
+jQuery.prototype.text = function(newText) {
+    if (newText) {
+        this.each(element => element.innerText = newText);
+        return this;
+    }
+    return this.elements[0].innerText;
+}
 const $ = (e) => new jQuery(e);
+
+if (Boolean(localStorage.getItem('yourcity'))) {
+    $('#subdiv1').hide();
+    $('#subdiv2').html('Теперь мы знаем ваш город. Это ' + localStorage.getItem('yourcity'));
+    $('#subdiv2').show();
+}
 
 yourcity.value = localStorage.getItem('yourcity');
 yourcity.oninput = () => localStorage.setItem('yourcity', yourcity.value);
